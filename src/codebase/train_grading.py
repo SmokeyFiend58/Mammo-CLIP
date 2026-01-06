bd4f9e2f3e3db538ae227a71feb42c2c1cd8ce0d
Louis Robson <louis.robson.uk@gmail.com>
2026-01-06 14:05:45 +0000

    Adding training stage for multi head classification

--- src/codebase/train_grading.py
+++ src/codebase/train_grading.py
@@ -13,4 +13,38 @@ from sklearn.metrics import f1_score, accuracy_score
 from torch.utils.tensorboard import SummaryWriter
 
 from utils import seed_all
-from breast
+from breastclip.model.modules.image_encoder import SwinTransformer_Mammo
+
+from breastclip.data.data_utils import load_transform
+
+warnings.filterwarnings("ignore")
+
+class MultiHeadSwin(nn.Module):
+    def __init__(self, encoder_name, img_size, num_density = 4, num_birads = 5):
+        super().__init__()
+
+    
+    #load swin backbone
+    
+        self.encoder = SwinTransformer_Mammo(
+            name = encoder_name,
+            pretrained= True
+            img_size= img_size
+        )
+        inputDim = self.encoder.outDim
+
+        #head 1 density
+        self.head_density = nn.Linear(inputDim, num_density)
+        
+        # head 2 birads
+        self.head_birads = nn.Linear(inputDim, num_birads)
+        
+    def forward(self, x):
+        features = self.encoder(x)
+        dOut = self.head_density(features)
+        bOut = self.head_birads(features)
+        
+        return dOut, bOut
+    
+#dataset handler
+
