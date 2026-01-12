2fa8edade34745090d7af683f1adbe48b3a62237
Louis Robson <louis.robson.uk@gmail.com>
2026-01-12 13:51:46 +0000

    Changing augmentation to be more aggressive to less dominant classes to make a more representative dataset

--- src/codebase/breastclip/data/data_utils.py
+++ src/codebase/breastclip/data/data_utils.py
@@ -23,79 +23,45 @@ def load_tokenizer(source, pretrained_model_name_or_path, cache_dir, **kwargs):
 
 # for images
 
-def load_transform(split: str = "train", transform_config: Dict = None):
-    img_size = 1344 # force swin resolution
-    if split == "train":
-        return albu.Compose([
-            
-            #resize
-            albu.Resize(height=img_size, width=img_size),
-            
-            #flip horizontal, flipping vertically does not make sense medically due to breast sagging
-            #flip with a probability of 0.5
-            albu.HorizontalFlip(p=0.5),
-            
-            #texture augmentation
-            albu.RandomBrightnessContrast(brightness_limit= 0.1, contrast_limit=0.1, p=0.5),
-            albu.OneOf([
+#data augmentation: need standard augmentation for dominant classes, and increase data size for weaker classes
+
+def get_density_augmentation(img_size = 1344):
+    base_Augmentation = [albu.Resize(height=img_size, width=img_size),
+                        albu.HorizontalFlip(p=0.5),
+                        albu.Normalize(mean = (0.485, 0.465, 0.406), std=(0.229, 0.224, 0.225)),
+                        ToTensorV2(),]
+    #mild texture change for dominant class
+    dominant_pipeline = albu.Compose([
+        albu.Resize(height=img_size, width=img_size),
+        albu.HorizontalFlip(p=0.5),
+        albu.RandomBrightnessContrast(brightness_limit= 0.1, contrast_limit=0.1, p=0.5),
+        albu.Normalize(mean = (0.485, 0.465, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),])
+    #heavy texture augmentation
+    #jitter, noise and blur to force robustness
+    
+    rare_pipeline = albu.Compose([
+        albu.Resize(height=img_size, width=img_size),
+        albu.HorizontalFlip(p=0.5),
+        albu.RandomBrightnessContrast(brightness_limit= 0.1, contrast_limit=0.1, p=0.5),
+        albu.OneOf([
                 albu.GaussianBlur(blur_limit=(3,5), p=0.5),
                 albu.Sharpen(alpha=(0.2, 0.5), lightness=(0.5,1.0), p = 0.5),
-                
-            ], p = 0.4),
-            
-            albu.GaussNoise(std_range=(0.2,0.44), p = 0.4),
-            
-            albu.Normalize(mean = (0.485, 0.465, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ])
-    else:
-        #valid / test/ aug
-        return albu.Compose([
-            albu.Resize(height=img_size, width=img_size),
-            albu.Normalize(mean= (0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ])
-    #this is contradicts my propsal, i need to implement a density aware augmentation
-    """assert split in {"train", "valid", "test", "aug"}
-    transforms = transform_config[split]
-    if split == "train":
-        if (transforms["Resize"]["size_h"] == 512 or transforms["Resize"]["size_h"] == 224) and (
-                transforms["Resize"]["size_w"] == 512 or transforms["Resize"]["size_w"] == 224):
-            return Compose([
-                Resize(width=transforms["Resize"]["size_h"], height=transforms["Resize"]["size_w"]),
-                HorizontalFlip(),
-                VerticalFlip(),
-                Affine(
-                    rotate=transforms["transform"]["affine_transform_degree"],
-                    translate_percent=transforms["transform"]["affine_translate_percent"],
-                    scale=transforms["transform"]["affine_scale"],
-                    shear=transforms["transform"]["affine_shear"]
-                ),
-                ElasticTransform(
-                    alpha=transforms["transform"]["elastic_transform_alpha"],
-                    sigma=transforms["transform"]["elastic_transform_sigma"]
-                )
-            ], p=transforms["transform"]["p"]
-            )
-        else:
-            return Compose([
-                HorizontalFlip(),
-                VerticalFlip(),
-                Affine(
-                    rotate=transforms["transform"]["affine_transform_degree"],
-                    translate_percent=transforms["transform"]["affine_translate_percent"],
-                    scale=transforms["transform"]["affine_scale"],
-                    shear=transforms["transform"]["affine_shear"]
-                ),
-                ElasticTransform(
-                    alpha=transforms["transform"]["elastic_transform_alpha"],
-                    sigma=transforms["transform"]["elastic_transform_sigma"]
-                )
-            ], p=transforms["transform"]["p"]
-            )
-    elif split == "valid":
-        if transforms["Resize"]["size_h"] == 512 and transforms["Resize"]["size_w"] == 512:
-            return Compose([
-                Resize(width=transforms["Resize"]["size_h"], height=transforms["Resize"]["size_w"])
-            ])
-            """
+                ], p = 0.4),
+        albu.GaussNoise(std_range=(0.2,0.44), p = 0.4),    
+        albu.Normalize(mean = (0.485, 0.465, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),])
+    
+    validation_pipeline = albu.Compose(base_Augmentation)
+    
+    return {"common": dominant_pipeline, "rare": rare_pipeline, "valid": validation_pipeline}
+
+
+def load_transform(split: str = "train", transform_config: Dict = None):
+    img_size = 1344 # force swin resolution
+    tfms = get_density_augmentation()
+    if split == "valid" or split == "test":
+        return tfms["valid"]
+    return tfms["common"]
+
+        
