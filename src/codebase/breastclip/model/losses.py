e8804fa4fe636667f09d09bb42502d6cf90c8e43
Louis Robson <louis.robson.uk@gmail.com>
2026-01-22 12:37:49 +0000

    Added new loss functions for latent space partitoning and density percent

--- src/codebase/breastclip/model/losses.py
+++ src/codebase/breastclip/model/losses.py
@@ -24,7 +24,20 @@ class OrdinalRegressionLoss(nn.Module):
         
         loss = self.bce(logits, ordinalTargets)
         return loss.sum(dim=1).mean()
-
+class GaussianUncertaintyLoss(nn.Module):
+    #regression loss for density percent
+    def __init__(self):
+        super().__init__()
+        self.nll = nn.GaussianNLLLoss()
+    def forward(self, predMean, pred_log_var, target):
+        #pred log.. comes from the network
+        var = torch.exp(pred_log_var)
+        #ensuring variance is positive
+        
+        if target.dim() == 1:
+            target = target.view(-1,1)
+        return self.nll(predMean, target, var)
+    
 class DensityMSELoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -32,6 +45,26 @@ class DensityMSELoss(nn.Module):
     def forward(self, logits, targets):
         targetsFloat = targets.float().view(-1,1)
         return self.mse(logits, targetsFloat)
+    
+class CentreLoss(nn.Module):
+    # penalties on the distance between latent features and class centres
+    def __init__(self, num_classes, feat_dim, device = 'cuda'):
+        super().__init__()
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.device = device
+        
+        #lernable parameter(different from the model weights) 
+        self.centres == nn.Parameter(torch.randn(num_classes, feat_dim).to(device))
+        
+    def forward(self, features, labels):
+        batch_size = features.size(0)
+        
+        centres_batch = self.centres.index_select(0, labels.long())
+        
+        loss = (features - centres_batch).pow(2).sum() / 2.0 / batch_size
+        return loss
+    
     
     
     
